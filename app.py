df52e9fd7cdc3ab49a8c710a26ff07c86cd8bb84
Sahana Ramesh <41955557+sahanaramesh09@users.noreply.github.com>
2021-07-15 19:47:19 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+# your code 
+import json
+from flask import Flask, request
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import re
+import nltk
+from nltk.corpus import stopwords
+import seaborn as sns
+import matplotlib.pyplot as pl
+import joblib
+
+def Clean(text):
+    text=text.split()
+    text=[i.lower() for i in text if i.lower() not in stopwords.words('english')]
+    text=' '.join(text)
+    text=re.sub('[^A-Za-z0-9]+',' ',text)
+    text=text.lower()
+    return text
+
+def Load_Predict(text):
+    tfidf = joblib.load("tfidf_model")
+    X_pred = tfidf.transform([text]).todense()
+    clf = joblib.load("LR_model")
+    prediction = clf.predict(X_pred)
+    return prediction[0]
+
+# create an instance of Flask
+app = Flask(__name__)
+
+
+
+# Define a post method for our API.
+@app.route('/classify', methods=['POST'])
+def classify():
+    text=request.form['text']
+    text = Clean(text)
+    prediction = Load_Predict(text)
+    response = json.dumps(prediction)
+    return response
+
+
+if __name__=="__main__":
+    app.run(debug=True)
